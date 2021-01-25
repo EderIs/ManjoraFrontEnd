9ae72d3967f32209ea82f1d4fd4ac00164f97196
Alejandro Rodriguez <alejandro.rodriguezh1879@gmail.com>
2021-01-25 11:18:44 -0600

    Actualizar Titulo
    Actualizar e insertar titulo en un formulario

--- src/app/views/contacto/titulo/lista-titulo.component.ts
+++ src/app/views/contacto/titulo/lista-titulo.component.ts
@@ -1,14 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { TituloService } from '../../../service/titulo.service';
+import { Titulo } from '../../../models/titulo';
 
 @Component({
   templateUrl: 'lista-titulo.component.html'
 })
 
 export class listaTituloComponent implements OnInit{
-  
+  titulos: Titulo[] = [];
+  constructor(
+    private tituloService: TituloService
+    ) { }
+
   ngOnInit(): void {
-    
+    this.cargarTitulos();
+  }
+
+  cargarTitulos(): void {
+    this.tituloService.lista().subscribe(
+      data => {
+        this.titulos = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  borrar(id: number) {
+    this.tituloService.delete(id).subscribe(
+      data => {
+       console.log(data);
+        this.cargarTitulos();
+      },
+      err => {
+        console.log(err)
+      }
+    );
   }
 }
--- src/app/views/contacto/titulo/nuevoeditar-titulo.component.ts
+++ src/app/views/contacto/titulo/nuevoeditar-titulo.component.ts
@@ -1,14 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { TituloService } from '../../../service/titulo.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Titulo } from '../../../models/titulo';
 
 @Component({
   templateUrl: 'nuevoeditar-titulo.component.html'
 })
 
 export class NuevoEditarTituloComponent implements OnInit{
-  
+    titulo: string = "";
+    abreviatura: string = "";
+    titu: Titulo = null;
+    id: number = this.activatedRoute.snapshot.params.id;
+
+  constructor(
+    private tituloService: TituloService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ) { }
+
   ngOnInit(): void {
     
+    if (this.id != null){
+      this.tituloService.detail(this.id).subscribe(
+        data => {
+          this.titu = data;
+        },
+        err => {
+          console.log(err);
+          this.router.navigate(['/contacto/titulo/listaTitulo']);
+        }
+      );
+    }
+    
   }
+
+  onCreate(): void {
+    if(this.id!=null){
+      this.tituloService.update(this.id, this.titu).subscribe(
+        data => {
+          this.titu = data;},
+          err => {
+            console.log(err);
+            });
+       
+    }
+    else{
+      const titut = new Titulo(this.titulo, this.abreviatura);
+    this.tituloService.save(titut).subscribe(
+      response => {
+        console.log(response);
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+    }
+    
+    
+    this.router.navigate(['/contacto/titulo/listaTitulo']);
+    }
 }
